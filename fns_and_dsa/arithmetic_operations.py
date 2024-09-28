dc92b612337ff76e80e08479b080203f16480f66
Engy114 <140180137+Engy114@users.noreply.github.com>
2024-09-29 01:31:10 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,29 @@
+def perform_operation(num1, num2, operation):
+    """
+    Perform the specified arithmetic operation.
+
+    Parameters:
+    num1 (float): The first number.
+    num2 (float): The second number.
+    operation (string): The operation to perform ('add', 'subtract', 'multiply', 'divide').
+
+    Returns:
+    float or str: The result of the operation, or a message if invalid operation or division by zero.
+    """
+    # Perform the arithmetic operation based on the 'operation' parameter
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            # Check for division by zero
+            if num2 == 0:
+                return "Cannot divide by zero."
+            else:
+                return num1 / num2
+        case _:
+            return "Invalid operation."
  
