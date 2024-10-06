d3fe96f40837dadf4b0bfd39132f55bb22fbb41b
Engy114 <engyrafat114@gmail.com>
2024-10-06 16:50:47 +0300

    commit

--- python_introduction/hours_to_seconds.py
+++
@@ -1,9 +0,0 @@
-# Define the variable for hours
-hours = 2
-
-# Calculate the number of seconds in the given hours
-seconds = hours * 3600
-
-# Print the result in the desired format
-print(f"{hours} hour(s) is {seconds} seconds.")
-
